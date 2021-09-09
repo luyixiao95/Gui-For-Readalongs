87d9e5799c252fa0aabffa21dcfaf246dbec89ff
Darrel <schreiner.da@northeastern.edu>
2021-09-09 15:25:52 -0700

    language function and combobox feature

--- main.py
+++ main.py
@@ -1,22 +1,37 @@
 import sys
+from utility import getLangs
 
-from PyQt5.uic import loadUi
-from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QDialog, QApplication, QWidget, QStackedWidget
+import qtpy.QtWidgets as qtw 
+import qtpy.QtGui as qtg
+from qtpy.uic import loadUi
+from qtpy import QtWidgets
+from qtpy.QtWidgets import QDialog, QApplication, QWidget, QStackedWidget, QComboBox, QListWidget
 
-# created a class called welcome screen
+# from PyQt5.uic import loadUi
+# from PyQt5 import QtWidgets
+# from PyQt5.QtWidgets import QDialog, QApplication, QWidget, QStackedWidget, QComboBox
+
+class ListboxWidget(QListWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setAcceptDrops(True)
+
+
 class WelcomeScreen(QDialog):
     def __init__(self):
         super(WelcomeScreen, self).__init__()
         loadUi("welcomescreen.ui",self)
-        #self.login.clicked.connect(self.gotologin)
-        #self.create.clicked.connect(self.gotocreate)
 
-        # Combo Box
-        list_languages = ["Language","Eng","Fr","Dn"]
-        for lang in list_languages:
-            self.comboBox.addItem(lang)
+        # load files 
         
+        # Combo Box
+        self.mappingOptions = getLangs()
+        for lang in self.mappingOptions:
+            self.comboBox.addItem(lang)
+
+        #drag and drop List Widget
+        self.upload_text_files = ListboxWidget(self)
+        self.upload_text_files.setGeometry
 
 # main
 app = QApplication(sys.argv)
